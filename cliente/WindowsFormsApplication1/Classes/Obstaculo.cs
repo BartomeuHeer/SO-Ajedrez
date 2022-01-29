8a18ffd29e4fa7119f2fc5dba75797eb32f603b0
Bartomeu Heer <bheersalva@gmail.com>
2022-01-29 09:36:07 +0100

    Versió final

---
+++ cliente/WindowsFormsApplication1/Classes/Obstaculo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1.Classes
+{
+    class Obstaculo
+    {
+        public Obstaculo(PictureBox pb)
+        {
+            this.tocado = false;
+            this.img = pb;
+        }
+        private PictureBox img;
+        private bool tocado;
+
+        public PictureBox getImg()
+        {
+            return this.img;
+        }
+        public void setImg(PictureBox image)
+        {
+            this.img = image;
+        }
+        public bool getTocado()
+        {
+            return this.tocado;
+        }
+        public void setTocado(bool t)
+        {
+            this.tocado = t;
+        }
+    }
+}
